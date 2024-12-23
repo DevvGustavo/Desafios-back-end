b41484bfe5aafbc6314759f44959454f84adfc67
Pablo Gustavo <pablogustavo.omea@gmail.com>
2024-12-22 23:17:59 -0300

    Alterações: Criação das funcionalidades para gerar um 'token' para o usuario

--- Autenticacao/src/main/java/org/example/Usuarios/TokenUser.java
+++ Autenticacao/src/main/java/org/example/Usuarios/TokenUser.java
@@ -1,7 +1,42 @@
 package org.example.Usuarios;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
 public class TokenUser {
 /*Gerar de forma automatica um token para confirmar que o usuario
 * foi autenticado no sistema*/
+
+    private Random numeroAleatorio;
+    private int token;
+    private String sequencia;
+    private List<Character> listaDeNumeros;
+
+    protected void funcionalidadesDoToken(){
+        numeroAleatorio = new Random();
+        token = numeroAleatorio.nextInt(301);
+
+        /*Convertendo o número que o token recebe em String*/
+        sequencia = String.valueOf(token);
+        listaDeNumeros = new ArrayList<>();
+
+        for (char c : sequencia.toCharArray()) {
+            listaDeNumeros.add(c);
+        }
+
+        //Embaralha os números
+        Collections.shuffle(listaDeNumeros);
+
+        // Converter a lista embaralhada de volta para uma String
+        StringBuilder tokenConvertido = new StringBuilder();
+
+        for (char c : listaDeNumeros) {
+
+            tokenConvertido.append(c);
+        }
+    }
+
 
 }
