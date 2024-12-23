595d9339e5900244d0d34a9a7a4d3ec8fc2d6a2e
Pablo Gustavo <pablogustavo.omea@gmail.com>
2024-12-22 22:24:19 -0300

    Primeiro commit

---
+++ Autenticacao/src/main/java/org/example/Usuarios/TokenUser.java
@@ -0,0 +1,7 @@
+package org.example.Usuarios;
+
+public class TokenUser {
+/*Gerar de forma automatica um token para confirmar que o usuario
+* foi autenticado no sistema*/
+
+}
---
+++ Autenticacao/src/main/java/org/example/Usuarios/Usuario.java
@@ -0,0 +1,23 @@
+package org.example.Usuarios;
+
+public class Usuario {
+
+    private int cpf;
+    private String senha;
+
+    public int getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
